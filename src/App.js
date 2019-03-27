5d894a150a9a88df9392695cbfde94c01083834f
adam <bcuz@users.noreply.github.com>
2019-03-27 10:33:20 -0400

    error page for bad routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
-import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home, About, Contact, NotFound, Navigation } from './components';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Navigation />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
